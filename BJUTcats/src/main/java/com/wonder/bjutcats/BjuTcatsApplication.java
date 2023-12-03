cfe029a789d3560513897a1a63271cc52e88fcef
tensor <lryf0415@gmail.com>
2023-12-03 18:39:57 +0800

    v0.1 allbegin

---
+++ BJUTcats/src/main/java/com/wonder/bjutcats/BjuTcatsApplication.java
@@ -0,0 +1,13 @@
+package com.wonder.bjutcats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BjuTcatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BjuTcatsApplication.class, args);
+    }
+
+}
---
+++ BJUTcats/src/test/java/com/wonder/bjutcats/BjuTcatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wonder.bjutcats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BjuTcatsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
